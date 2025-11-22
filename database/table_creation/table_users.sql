99328c29b923947a0bd101c7d82f580cb17c27ce
mek <mehmet73emn@gmail.com>
2025-11-22 19:12:57 +0300

    update

---
+++ database/table_creation/table_users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    avatar_url VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
